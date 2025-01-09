55afbb6b420cc4b998f190ae8a9888f69d4bdf44
Super Dev <superdev1031@gmail.com>
2025-01-09 10:09:22 -0500

    show image

--- src/components/CarDetail.js
+++ src/components/CarDetail.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import Item from './Item';
 import ItemSection from './ItemSection';
 
 const CarDetail = ({brand}) => {
+  const {headerContainer, headerText, imageStyle} = styles;
   return (
     <Item>
       <ItemSection>
-        <Text>{brand.model[0].name}</Text>
+        <View style={headerContainer}>
+          <Text style={headerText}>{brand.brand}</Text>
+          <Text style={headerText}>{brand.model[0].name}</Text>
+        </View>
+      </ItemSection>
+      <ItemSection>
+        <Image style={imageStyle} source={{uri: brand.model[0].image}} />
       </ItemSection>
     </Item>
   );
+};
+
+const styles = {
+  headerContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: '500',
+    textTransform: 'uppercase',
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: 0,
+  },
 };
 
 export default CarDetail;
